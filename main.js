d57b797e9c2e31cfe6c14e8a02f714743a5532f0
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2023-10-26 19:34:33 +0300

    Add photo animation

--- main.js
+++ main.js
@@ -9,3 +9,29 @@ $('.header_themeChanger').click(function(){
     }
 
 })
+let photo = 'defolt';
+
+$('#myPhoto').click(function() {
+    if (photo === 'defolt') {
+        $("#myPhoto").css("opacity", 0.9);
+        setTimeout(function() {
+            $('#myPhoto').attr('src', './img/My photo 2.png')
+            $("#myPhoto").css("opacity", 1);
+        }, 200);
+        photo = 'secondOne';
+    } else if (photo === 'secondOne') {
+        $("#myPhoto").css("opacity", 0.9);
+        setTimeout(function() {
+            $('#myPhoto').attr('src', './img/My photo 3.png')
+            $("#myPhoto").css("opacity", 1);
+        }, 200);
+        photo = 'thirdOne';
+    } else if (photo === 'thirdOne') {
+        $("#myPhoto").css("opacity", 0.9);
+        setTimeout(function() {
+            $('#myPhoto').attr('src', './img/My photo.png')
+            $("#myPhoto").css("opacity", 1);
+        }, 200);
+        photo = 'defolt';
+    }
+});
